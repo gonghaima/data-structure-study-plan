3824f7261264d1c5aed166f266b04aa895272ed9
Steven Gong <gonghaima@hotmail.com>
2025-10-09 11:12:55 +1100

    459. Repeated Substring Pattern - intuitive solution

--- week1/Repeated_Substring_Pattern/solution.js
+++ week1/Repeated_Substring_Pattern/solution.js
@@ -2,5 +2,14 @@
  * @param {string} s
  * @return {boolean}
  */
-var repeatedSubstringPattern = function (s) {};
+var repeatedSubstringPattern = function (s) {
+  for (let i = 1; i <= s.length / 2; i++) {
+    if (s.length % i === 0) {
+      let sub = s.substring(0, i);
+      let repeated = sub.repeat(s.length / i);
+      if (repeated === s) return true;
+    }
+  }
+  return false;
+};
 module.exports = repeatedSubstringPattern;
