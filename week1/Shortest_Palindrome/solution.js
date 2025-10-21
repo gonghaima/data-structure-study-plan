6b9edd48a540c1625b3dde72535369fd84ee9ccd
Steven Gong <gonghaima@hotmail.com>
2025-10-21 14:26:32 +1100

    214. Shortest Palindrome - intuitive solution

--- week1/Shortest_Palindrome/solution.js
+++ week1/Shortest_Palindrome/solution.js
@@ -2,6 +2,14 @@
  * @param {string} s
  * @return {string}
  */
-var shortestPalindrome = function (s) {};
+var shortestPalindrome = function (s) {
+  let rev = s.split('').reverse().join('');
+  for (let i = 0; i < s.length; i++) {
+    if (s.substring(0, s.length - i) === rev.substring(i)) {
+      return rev.substring(0, i) + s;
+    }
+  }
+  return '';
+};
 
 module.exports = shortestPalindrome;
