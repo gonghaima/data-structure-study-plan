4f7378284d54d6b0dbcd47623bcec139e92d29df
Steven Gong <gonghaima@hotmail.com>
2025-10-31 16:05:30 +1100

    30. Substring with Concatenation of All Words - setup

---
+++ week1/SubstringWithConcatenationOfAllWords/solution.js
@@ -0,0 +1,14 @@
+// Solution for 'Substring with Concatenation of All Words'
+// Implement your function here
+
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = function (s, words) {
+  // TODO: implement
+  return null;
+};
+
+module.exports = findSubstring;
---
+++ week1/SubstringWithConcatenationOfAllWords/testdata.js
@@ -0,0 +1,17 @@
+// Test data for 'Substring with Concatenation of All Words'
+
+module.exports = [
+  {
+    input: { s: "barfoothefoobarman", words: ["foo","bar"] },
+    output: [0,9]
+  },
+  {
+    input: { s: "wordgoodgoodgoodbestword", words: ["word","good","best","word"] },
+    output: []
+  },
+  {
+    input: { s: "barfoofoobarthefoobarman", words: ["bar","foo","the"] },
+    output: [6,9,12]
+  },
+  // Add more edge cases as needed
+];
