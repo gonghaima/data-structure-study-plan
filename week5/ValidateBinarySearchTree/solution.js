37437c7b9f24de7a25210d5b4f65c389c8d9b10d
Steven Gong <gonghaima@hotmail.com>
2026-03-23 11:38:45 +1100

    98. Validate Binary Search Tree - dfs

--- week5/ValidateBinarySearchTree/solution.js
+++ week5/ValidateBinarySearchTree/solution.js
@@ -4,8 +4,10 @@
  */
 var isValidBST = function(root) {
     // TODO: Implement your solution here
-    
-    return false;
+    if (!root) return true;
+    if (root.left && root.left.val >= root.val) return false;
+    if (root.right && root.right.val <= root.val) return false;
+    return isValidBST(root.left) && isValidBST(root.right);
 };
 
 module.exports = isValidBST;
