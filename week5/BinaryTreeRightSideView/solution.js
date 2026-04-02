9ba34d558b0554d682a8ffc58b65ca8dea82de52
Steven Gong <gonghaima@hotmail.com>
2026-04-02 16:26:39 +1100

    199. Binary Tree Right Side View - my solution - bfs

--- week5/BinaryTreeRightSideView/solution.js
+++ week5/BinaryTreeRightSideView/solution.js
@@ -2,8 +2,21 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var rightSideView = function(root) {
-    // TODO: Implement your solution here
+var rightSideView = function (root) {
+  // TODO: Implement your solution here
+  if (!root) return [];
+  const result = [];
+  let queue = [root];
+  while (queue.length) {
+    let next = [];
+    if (queue.length) result.push(queue[queue.length - 1]?.val);
+    for (let i = 0; i < queue.length; i++) {
+      if (queue[i].left) next.push(queue[i].left);
+      if (queue[i].right) next.push(queue[i].right);
+    }
+    queue = next;
+  }
+  return result;
 };
 
 module.exports = rightSideView;
