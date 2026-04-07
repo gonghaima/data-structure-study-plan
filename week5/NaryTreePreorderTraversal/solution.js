5948118835e141409b3769211c52b828400eaeff
Steven Gong <gonghaima@hotmail.com>
2026-04-08 09:54:28 +1000

    589. N-ary Tree Preorder Traversal - my solution

--- week5/NaryTreePreorderTraversal/solution.js
+++ week5/NaryTreePreorderTraversal/solution.js
@@ -4,6 +4,21 @@
  */
 var preorder = function(root) {
     // TODO: Implement your solution here
+    let result = [];
+
+    function dfs(node) {
+        if (!node) return;
+
+        result.push(node.val); // Process the current node first
+
+        // Recursively process all children
+        for (let child of node.children) {
+            dfs(child);
+        }
+    }
+
+    dfs(root);
+    return result;
 };
 
 module.exports = preorder;
