2c94caadeb3b5fbdb6a98405569ae5ad882bd4d8
Steven Gong <gonghaima@hotmail.com>
2026-04-16 11:29:39 +1000

    429. N-ary Tree Level Order Traversal - my solution

--- week5/NaryTreeLevelOrderTraversal/solution.js
+++ week5/NaryTreeLevelOrderTraversal/solution.js
@@ -4,6 +4,31 @@
  */
 var levelOrder = function (root) {
     // TODO: Implement your solution here
+    const result = [];
+    let queue = [root];
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        const currentLevel = [];
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            if (!node) continue;
+
+            currentLevel.push(node.val);
+
+            // Add all children to the queue
+            for (const child of node.children) {
+                queue.push(child);
+            }
+        }
+
+        if (currentLevel.length > 0) {
+            result.push(currentLevel);
+        }
+    }
+
+    return result;
 };
 
 module.exports = levelOrder;
