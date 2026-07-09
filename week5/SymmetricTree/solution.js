8b4a260acd6d918bf54a8b0cc7daa42c9c1dcc9a
Steven Gong <gonghaima@hotmail.com>
2026-07-09 16:14:44 +1000

    101. Symmetric Tree - simple solution

--- week5/SymmetricTree/solution.js
+++ week5/SymmetricTree/solution.js
@@ -2,8 +2,26 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function(root) {
-    // TODO: Implement your solution here
+var isSymmetric = function (root) {
+  // An empty tree is technically symmetric
+  if (!root) return true;
+
+  function isMirror(left, right) {
+    // Base case: both nodes are null
+    if (!left && !right) return true;
+
+    // One node is null, but the other isn't
+    if (!left || !right) return false;
+
+    // Values don't match, or subtrees aren't mirrors
+    return (
+      left.val === right.val &&
+      isMirror(left.left, right.right) &&
+      isMirror(left.right, right.left)
+    );
+  }
+
+  return isMirror(root.left, root.right);
 };
 
 module.exports = isSymmetric;
