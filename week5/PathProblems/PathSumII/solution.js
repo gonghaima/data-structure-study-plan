bedae7d3e342e30245969cfa0b64137ed711dde9
Steven Gong <gonghaima@hotmail.com>
2026-09-26 11:05:36 +1000

    113. Path Sum II - backtrack - dfs

--- week5/PathProblems/PathSumII/solution.js
+++ week5/PathProblems/PathSumII/solution.js
@@ -4,7 +4,29 @@
  * @return {number[][]}
  */
 var pathSum = function (root, targetSum) {
-  // TODO: Implement your solution here
+  const result = [];
+  const path = [];
+
+  function dfs(node, remaining) {
+    if (!node) return;
+
+    path.push(node.val);
+    const nextRemaining = remaining - node.val;
+
+    if (!node.left && !node.right) {
+      if (nextRemaining === 0) {
+        result.push([...path]);
+      }
+    } else {
+      dfs(node.left, nextRemaining);
+      dfs(node.right, nextRemaining);
+    }
+
+    path.pop();
+  }
+
+  dfs(root, targetSum);
+  return result;
 };
 
 module.exports = pathSum;
